75065f5bb15ed1df3b2f1e1e16bd3635f292b3c1
pumelotea <zf_722@yeah.net>
2021-07-04 12:34:21 +0800

    Initial commit

---
+++ config/api.js
@@ -0,0 +1,11 @@
+window.appAPI = {
+  dev: {
+    server: 'https://www.jpldx.com/vue-sfc/api'
+  },
+  prod: {
+    server: 'https://www.jpldx.com/vue-sfc/api'
+  },
+  test: {
+    server: 'https://www.jpldx.com/vue-sfc/api'
+  }
+}
